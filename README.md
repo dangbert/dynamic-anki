87f729f0ec4bad72e78b28950395130a1b4d3207
Daniel Engbert <end1@umbc.edu>
2022-01-23 13:00:21 -0500

    Remove arrow syntax to prevent need for a separate namespace.

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # anki-dynamic
 
-A js lib that attempts to make Anki flashcards more "dynamic" by varying their content.
+A js lib that attempts to make Anki flashcards more "dynamic" by varying their content, font size, etc.  The ultimate goal being to improve learning; ideally for each review of a flashcard to not only review what you already know, but hopefully add a little bit of new knowledge / connect new ideas on top of what you learend previously.
 
-For example, each time a given flashcard is shown, we can replace the word (e.g. "hola") with a random sentence containing that word.
+For example, for language learning, each time a given flashcard is shown, we can replace the word (e.g. "hola") with a random sentence containing that word.
 
 This lib interfaces with a personal API endpoint I built that returns a list of random sentences (sourced from misc ebooks) containing a desired word or phrase.
 
@@ -30,9 +30,13 @@ Supported languages for vocab sentences lookup:
 * es
 * pt
 
+Note: I added el and tr but they don't currently work with this lib (probably an issue with char encoding).
+
 ## Dev Notes:
 
 ````bash
 # run unit tests:
 yarn mocha
 ````
+
+You can also open `_test.html` in your browser to simulate rendering a flashcard (for quick testing of this js lib).  But ultimately you should open Anki and view a few cards back to back (while viewing the browser dev console for errors).
--- _dynamicCard.js
+++ _dynamicCard.js
@@ -1,34 +1,34 @@
-// wrap everything in braces to put it in another namespace
-// otherwise each card will try to redefine these functions...
-
-{
 /**
  * get text content of front of anki card (just inner html values).
  * https://www.geeksforgeeks.org/how-to-get-all-html-content-from-domparser-excluding-the-outer-body-tag/
  */
-const getFront = () => {
+function getFront() {
   let val = $("#front").html();
   const parser = new DOMParser();
   let doc = parser.parseFromString(val, "text/html");
 
-  val = doc.all[0].textContent.trim();
+  val = doc.all[0].textContent;
   //console.log("parsed val = "); console.log(val);
-  return val;
-};
+  return trimContents(val);
+}
 
-const getSentences = async val => {
+function trimContents(s) {
+  return s.trim();
+}
+
+async function fetchSentences(val) {
   //const data = await $.get('http://localhost:5000/api/tools/vocab/sentences', {phrase: val, offset: 0 });
   const data = await $.get('https://beta.engbert.me/api/tools/vocab/sentences', {phrase: val, offset: 0 });
   console.log('api call result:');
   console.log(data);
   return data.sentences;
-};
+}
 
 /**
  * format a sentence by bolding the desired indices.
  * based on https://github.com/dangbert/personalpedia/blob/master/react-app/src/components/ToolsApp/Vocab.tsx#L142
  */
-const formattedText = (text, bold) => {
+function formattedText(text, bold) {
   let index = 0;
   const nodes = [];
   for (let b of bold) {
@@ -57,32 +57,10 @@ const formattedText = (text, bold) => {
     </span>`
   ));
   return tmp.join(''); // one combined string
-};
+}
 
 
-/*
-// https://www.reddit.com/6u1kvm
-var sheet = window.document.styleSheets[0];
-//sheet.insertRule('strong { color: red; }'
-//sheet.cssRules.length);
-
-style = document.getElementById("front").style;
-style.fontFamily = "random_" + Math.floor(Math.random() * 36);
-//style.fontSize = "60px";
-*/
-
-
-// front template:
-// <div id="front">{{Front}}</div>
-
-// back template:
-/*
-<div id="front">{{FrontSide}}</div>
-<hr id=answer>
-<div id="back">{{Back}}</div>
-*/
-
-const randomFont = (id) => {
+function randomFont(id) {
   // https://www.reddit.com/6u1kvm
   var sheet = window.document.styleSheets[0];
   //sheet.insertRule('strong { color: red; }'
@@ -96,14 +74,14 @@ const randomFont = (id) => {
   const fontSize = 14 + Math.floor(Math.random() * 8);
   console.log("setting fontsize to " + fontSize);
   style.fontSize = `${fontSize}px`;
-};
+}
 
 (async () => {
   randomFont('front');
   randomFont('back');
 
   const front = getFront();
-  const sentences = await getSentences(front);
+  const sentences = await fetchSentences(front);
   console.log("sentences = ");
   console.log(sentences);
 
@@ -115,4 +93,3 @@ const randomFont = (id) => {
     $('#front').html(formattedText(s.text, s.bold));
   }
 })();
-}
